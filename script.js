e1f4a3053881800d9e311a3aed07288e5e818a92
maryam <marmaraymansh2017@gmail.com>
2022-08-13 14:48:50 +0300

    done cw6

---
+++ script.js
@@ -0,0 +1,23 @@
+alert("💎👑Welcome to my site which will calculate your grade!!💎👑");
+alert("🦋maryoma🦋")
+
+let grade = prompt ("Write your grade numbers");
+
+console.log(grade);
+
+if(grade >= 90) {
+    console.log( "لقد حصلت على امتياز 🥳");
+} else if (grade >= 80) {
+    console.log("لقد حصلت على جيد جداً🤩");
+} else if (grade >= 70) {
+    console.log("لقد حصلت على جيد🙂");
+} else if (grade >= 60) {
+    console.log("لقد حصلت على مقبول😕");
+}else if (grade >= 50){
+    console.log("لقد حصلت على ضعيف☹️");
+}else {
+    console.log("راسب💔");
+}
+
+//جافاسكربت ليست جافا
+
